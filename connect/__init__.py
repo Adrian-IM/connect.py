72a9dccedbaadb5241bcfd6d46e45b26835a1dd3
Adrian <adrian.gonzalezalonso@ingrammicro.com>
2019-11-05 12:00:56 +0100

    first upload

---
+++ connect/__init__.py
@@ -0,0 +1,20 @@
+from connect.autogen import connect_Collection as Collection
+from connect.autogen import connect_Config as Config
+from connect.autogen import connect_Dictionary as Dictionary
+from connect.autogen import connect_Env as Env
+from connect.autogen import connect_Inflection as Inflection
+from connect.autogen import connect_Logger as Logger
+from connect.autogen import connect_LoggerWriter as LoggerWriter
+from connect.autogen import connect_Processor as Processor
+
+
+__all__ = [
+    'Collection',
+    'Config',
+    'Dictionary',
+    'Env',
+    'Inflection',
+    'Logger',
+    'LoggerWriter',
+    'Processor',
+]
